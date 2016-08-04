5aa80627a797d685450a5b1d672ed29af7ea2af1
atliwen <499014099@qq.com>
2016-08-04 11:23:04 +0800

    自定义消费 接口

--- src/main/java/com/zjs/edi/mq/service/rocketmq/messagelistener/base/BaseMessageListenerConsumer.java
+++ src/main/java/com/zjs/edi/mq/service/rocketmq/messagelistener/base/BaseMessageListenerConsumer.java
@@ -6,7 +6,7 @@ import com.alibaba.rocketmq.common.message.MessageExt;
 
 /**
 * <p>Title: BaseMessageListenerConsumer </p>
-* <p>@Description: 自定义消费 接口      继承该接口   实现消费方法   </p>
+* <p>@Description: 自定义消费 接口     继承该接口   实现消费方法   </p>
 * <p>Company:  </p>
 * @author 李文
 * @date   2016年8月1日 上午10:05:04 
